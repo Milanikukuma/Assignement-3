83fbd5004ac84798921c7acacd20ac56d9d22fb9
Milanikukuma <kukumamilani62@gmail.com>
2023-12-12 10:02:20 +0200

    First Commit

---
+++ NumberConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToSpokenWords
+{
+    internal class NumberConverter
+    {
+        private static string[] units = {
+            "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"
+        };
+
+        private static string[] teens = {
+            "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+        };
+
+        private static string[] tens = {
+            "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+        };
+
+        public static string ConvertToWords(int number)
+        {
+            if (number < 0 || number > 9999)
+            {
+                return "Number out of range or negative";
+            }
+
+            if (number < 10)
+            {
+                return units[number];
+            }
+            else if (number < 20)
+            {
+                return teens[number - 10];
+            }
+            else if (number < 100)
+            {
+                return tens[number / 10] + ((number % 10 != 0) ? " " + units[number % 10] : "");
+            }
+            else if (number < 1000)
+            {
+                return units[number / 100] + " Hundred" + ((number % 100 != 0) ? " and " + ConvertToWords(number % 100) : "");
+            }
+            else
+            {
+                return units[number / 1000] + " Thousand" + ((number % 1000 != 0) ? " " + ConvertToWords(number % 1000) : "");
+            }
+        }
+    }     
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NumberToSpokenWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number (0 to 9999):");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                string words = NumberConverter.ConvertToWords(number);
+                Console.WriteLine($"Number in words: {words}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+    }
+}
